7ee6fe26cd92757b1e8d6744418e8b4c644e53d7
BTS <bts7623@gmail.com>
2026-05-21 20:08:30 +0900

    chore: build.bat 파일명 버전만 표기, 구버전 exe 자동 삭제

--- build.bat
+++ build.bat
@@ -1,13 +1,16 @@
 @echo off
 set VER=v1.0.0
-for /f "tokens=*" %%i in ('powershell -command "Get-Date -Format 'yyyyMMdd_HHmmss'"') do set TS=%%i
-set NAME=posture_reminder_%VER%_%TS%
+set NAME=posture_reminder_%VER%
 
 pip install pyinstaller --quiet
+
+if exist dist\posture_reminder_v*.exe del /q dist\posture_reminder_v*.exe
+
 python -m PyInstaller --onefile --noconsole --name %NAME% ^
     --hidden-import pystray._win32 ^
     --hidden-import screeninfo.enumerators.windows_multimon ^
     posture_reminder.py
+
 if exist %NAME%.spec del %NAME%.spec
 if exist build rmdir /s /q build
 
